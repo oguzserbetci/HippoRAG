c0bbc7a422ed9a0f17b640c4b78460a0091b996f
Oguz Serbetci <oguz.serbetci@informatik.hu-berlin.de>
2025-09-03 14:53:17 +0200

    add sentencebert baseline

---
+++ baseline_pmc.py
@@ -0,0 +1,82 @@
+from itertools import islice
+import os
+import pathlib
+import time
+import srsly
+import polars as pl
+
+from beir.retrieval import models
+from beir.retrieval.evaluation import EvaluateRetrieval
+from beir.retrieval.search.dense import DenseRetrievalExactSearch as DRES
+
+def main():
+    queries = srsly.read_jsonl(
+        "/vol/wbi/wbi_stud/wbi-datasets/PMC-Patients/ReCDS_benchmark/queries/dev_queries.jsonl"
+    )
+    queries = {s["_id"]: s["text"] for s in queries}
+    corpus = srsly.read_jsonl(
+            "/vol/wbi/wbi_stud/wbi-datasets/PMC-Patients/ReCDS_benchmark/PPR/corpus.jsonl"
+        )
+    corpus = {s["_id"]: s for s in corpus}
+
+    qrels = pl.read_csv(
+        "/vol/wbi/wbi_stud/wbi-datasets/PMC-Patients/ReCDS_benchmark/PPR/qrels_dev.tsv",
+        separator="\t",
+    )
+    qrels = {
+        row["query-id"]: {c_id: 1 for c_id in row["corpus-id"]}
+        for row in qrels.group_by("query-id").agg(pl.col("corpus-id")).rows(named=True)
+    }
+    queries = {k: v for k, v in queries.items() if k in qrels}
+
+    subcorpus = {}
+    for query_id, docs in qrels.items():
+        for doc_id in docs:
+            if doc_id in corpus:
+                subcorpus[doc_id] = corpus[doc_id]
+
+    # Uncomment for subcorpus
+    # corpus = subcorpus
+    print("Data is ready:", 'Corpus:',len(corpus), 'Queries:', len(queries), 'Qrels:', len(qrels))
+
+    # model_name_or_path = "Qwen/Qwen3-Embedding-8B"
+    # dense_model = models.HuggingFace(
+    #     model_path=model_name_or_path,
+    #     max_length=2048,
+    #     pooling='eos',
+    #     normalize=True,
+    #     prompts={"query": 'Given a bioclinical text, retrieve relevant passages that are most similar in clinical sense.',
+    #              "passage": ''},
+    # )
+    
+    model_name_or_path = "sentence-transformers/all-mpnet-base-v2"
+    dense_model = models.SentenceBERT(
+        model_name_or_path,
+        max_length=512,
+        trust_remote_code=True,
+    )
+
+    model = DRES(dense_model, batch_size=512, corpus_chunk_size=10000)
+    retriever = EvaluateRetrieval(
+        model, score_function="cos_sim", k_values=[1, 5, 10, 1000]
+    )
+
+    start_time = time.time()
+    results = retriever.encode_and_retrieve(
+        corpus=corpus,
+        queries=queries,
+        overwrite=True
+    )
+    end_time = time.time()
+
+    ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
+    mrr = retriever.evaluate_custom(qrels, results, k_values=retriever.k_values + [len(corpus)], metric='mrr')
+    print('Indexed and retrieved in', (end_time - start_time) / 60, 'minutes')
+    print("RECALL", recall)
+    print("PRECISION", precision)
+    print("NDCG", ndcg)
+    print("MAP", _map)
+    print("MRR", mrr)
+
+if __name__ == "__main__":
+    main()
